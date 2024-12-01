c019c02b7432ed429f8ed8b0fac15e79bb307d2c
jom-hedges <jom_hedges@proton.me>
2024-12-01 22:35:59 +0800

    added moduledoc

---
+++ lib/advent_of_code/day01.ex
@@ -0,0 +1,23 @@
+defmodule AdventOfCode.Day01 do
+  @moduledoc """
+  Day 1: Historian Hysteria
+
+  https://adventofcode.com/2024/day/1
+  """
+
+  @doc """
+  What is the distance between the lists?
+
+  ## Example
+
+    iex> list_1 = [3, 4, 2, 1, 3, 3]
+    iex> list_2 = [4, 3, 5, 3, 9, 3]
+    iex> Day01.part_1(list_1, list_2)
+    11
+
+  """
+  def part_1() do
+    #TODO: finish the problem
+  end
+
+end
